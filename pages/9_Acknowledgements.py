70ed134518a2bed33f7c9a5d9f9b89f41905a709
Sohel Modi <59948950+Sohel-coder@users.noreply.github.com>
2025-04-27 00:23:49 +0530

    Add files via upload

---
+++ pages/9_Acknowledgements.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+headline = 'Acknowledgements'
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://images5.alphacoders.com/438/438504.jpg');
+        background-attachment: fixed;
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.title(headline)
+st.markdown("""
+""", unsafe_allow_html=False)
+st.markdown("""
+**The success of this project is attributed to the dedication, expertise, and collaborative efforts of all the team members of Group 8**
+""", unsafe_allow_html=False)
+
+st.markdown("""
+""", unsafe_allow_html=False)
+st.markdown("""**Abhijeet Shravansing Rajput**""",unsafe_allow_html=False)
+st.markdown("""**Abhinandan Singh Baghel**""",unsafe_allow_html=False)
+st.markdown("""**Devansh Dhaval Mehta**""",unsafe_allow_html=False)
+st.markdown("""**Divya Sharma**""",unsafe_allow_html=False)
+st.markdown("""**Kamal Kant Tripathi**""",unsafe_allow_html=False)
+st.markdown("""**Patel Ujjaval Girishbhai**""",unsafe_allow_html=False)
+st.markdown("""**Sohel Samirkhan Modi**""",unsafe_allow_html=False)
+st.markdown("""**Vihsal Kumar**""",unsafe_allow_html=False)
