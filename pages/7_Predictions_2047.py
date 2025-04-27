42c5aa63febbbb1d0bc93a9a479cfeb3a08e307a
Sohel Modi <59948950+Sohel-coder@users.noreply.github.com>
2025-04-27 20:31:53 +0530

    Update 7_Predictions_2047.py

--- pages/7_Predictions_2047.py
+++ pages/7_Predictions_2047.py
@@ -21,7 +21,7 @@ def load_data():
     - Defence_budget_cleaned.csv
     """
     military_strength = pd.read_csv("data/2024_military_strength_by_country.csv")
-    defense_budget = pd.read_csv("data/Cleaned_Defence_budget.csv")
+    defense_budget = pd.read_csv("data/Cleaned_Defence_Budget.csv")
     return military_strength, defense_budget
 
 military_strength, defense_budget = load_data()
